800c5508c0e87a0190782e39d833c25557c347fe
Abhishek Tadkod <41908168+abhishektadkod@users.noreply.github.com>
2020-03-19 12:52:37 +0530

    new dash

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -1,10 +1,21 @@
 import React, { Component } from 'react'
+import Sidebar from './sidebar'
+import Home from './home'
 
 class Dashboard extends Component {
     render() {
         return (
             <div>
-                <h1>Hello</h1>
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-md-2 bg-dark">
+                            <Sidebar/>
+                        </div>
+                        <div class="col-md-10">
+                            <Home/>
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }

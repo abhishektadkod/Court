0d4aa20b14afcedd7841a15062e1bd7c854e01b4
abhishektadkod <abhishektadkod@gmail.com>
2020-04-29 15:39:02 +0530

    congig

---
+++ build/precache-manifest.da92d697456c309499780fd425dd5890.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53cff3a87c57f03157d9836b7d722f6f",
+    "url": "/Court/index.html"
+  },
+  {
+    "revision": "65fb34d050a294388d53",
+    "url": "/Court/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "555d9913581bdceb51f2",
+    "url": "/Court/static/css/main.1adba427.chunk.css"
+  },
+  {
+    "revision": "65fb34d050a294388d53",
+    "url": "/Court/static/js/2.bd20e050.chunk.js"
+  },
+  {
+    "revision": "5c2176e7db1837929debf21c65634f23",
+    "url": "/Court/static/js/2.bd20e050.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "555d9913581bdceb51f2",
+    "url": "/Court/static/js/main.6f7a9040.chunk.js"
+  },
+  {
+    "revision": "151fc6d4fe5b02497515",
+    "url": "/Court/static/js/runtime-main.76c7c083.js"
+  }
+]);
--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-export const SERVER_URL = "http://localhost:4000"
+export const SERVER_URL = "https://safe-taiga-19842.herokuapp.com"
 export const SOCKETIO_SERVER ="http://localhost:4001"
